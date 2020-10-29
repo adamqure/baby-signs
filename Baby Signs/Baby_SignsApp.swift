941d6c9f9520e7508c2a9fe2e5ac742d268ecf69
Adam Ure <adamqure@icloud.com>
2020-10-28 20:23:24 -0600

    Initial Commit

---
+++ Baby Signs/Baby_SignsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Baby_SignsApp.swift
+//  Baby Signs
+//
+//  Created by Adam Ure on 10/28/20.
+//
+
+import SwiftUI
+
+@main
+struct Baby_SignsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
